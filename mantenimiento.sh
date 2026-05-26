5036be00dd9a59d5b8095bdb5789fa912261a10c
Diego Ricardo Moreno Salazar <alu.22130860@correo.itlalaguna.edu.mx>
2026-05-26 01:19:26 -0600

    Add maintenance script for database reorganization

---
+++ mantenimiento.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Mantenimiento y Reindexación - AquaSmart source /home/db2inst1/sqllib/db2profile db2 connect to AQUASMRT
+echo "Reorganizando tablas e índices fragmentados..." db2 reorgchk update statistics on table all
+db2 disconnect AQUASMRT
+echo "Limpiando logs antiguos de auditoría..."
+db2audit prune date $(date --date="7 days ago" +%Y%m%d)
